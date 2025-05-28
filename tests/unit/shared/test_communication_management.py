04f16436171c3f619110a6a70c16f786573a6014
Steve Laing <steve.laing@gmail.com>
2025-05-28 14:42:49 +0100

    Merge message status handler CMAPI code into shared module
    This adds the GET /statuses calling code into the shared module along with unit tests.

--- tests/unit/shared/test_communication_management.py
+++ tests/unit/shared/test_communication_management.py
@@ -86,3 +86,80 @@ def test_generate_hmac_signature():
     hmac_signature = communication_management.generate_hmac_signature({"data": "data"})
 
     assert hmac_signature == "e2a0ce7f9e78746d86cbdb5ebcc9bae6eb25bfed844498d3f818ae5f975ef40f"
+
+
+def test_get_read_messages(monkeypatch):
+    monkeypatch.setenv("COMMGT_BASE_URL", "http://example.com")
+
+    with requests_mock.Mocker() as rm:
+        adapter = rm.get(
+            "http://example.com/statuses",
+            status_code=201,
+            json={
+                'status': 'success',
+                'response': 'message_batch_post_response',
+                'data': [{
+                    'channel': 'nhsapp',
+                    'channelStatus': 'delivered',
+                    'supplierStatus': 'read',
+                    'message_id': '2WL3qFTEFM0qMY8xjRbt1LIKCzM',
+                    'message_reference': '1642109b-69eb-447f-8f97-ab70a74f5db4'
+                }]
+            }
+        )
+
+        response_json = communication_management.get_read_messages("c3b8e0c4-5f3d-4a2b-8c7f-1a2e9d6f3b5c")
+
+        assert response_json["status"] == "success"
+        assert len(response_json["data"]) == 1
+        assert response_json["data"][0]["channel"] == "nhsapp"
+        assert response_json["data"][0]["channelStatus"] == "delivered"
+        assert response_json["data"][0]["supplierStatus"] == "read"
+        assert response_json["data"][0]["message_id"] == "2WL3qFTEFM0qMY8xjRbt1LIKCzM"
+        assert response_json["data"][0]["message_reference"] == "1642109b-69eb-447f-8f97-ab70a74f5db4"
+
+        assert adapter.called
+        assert adapter.call_count == 1
+        assert adapter.last_request.qs == {
+            "batchreference": ["c3b8e0c4-5f3d-4a2b-8c7f-1a2e9d6f3b5c"],
+            "channel": ["nhsapp"],
+            "supplierstatus": ["read"],
+        }
+
+
+def test_get_read_messages_no_data(monkeypatch):
+    monkeypatch.setenv("COMMGT_BASE_URL", "http://example.com")
+
+    with requests_mock.Mocker() as rm:
+        rm.get(
+            "http://example.com/statuses",
+            status_code=201,
+            json={
+                'status': 'success',
+                'data': []
+            }
+        )
+
+        response_json = communication_management.get_read_messages("c3b8e0c4-5f3d-4a2b-8c7f-1a2e9d6f3b5c")
+
+        assert response_json["status"] == "success"
+        assert response_json["data"] == []
+
+
+def test_get_read_messages_exception(monkeypatch):
+    monkeypatch.setenv("COMMGT_BASE_URL", "http://example.com")
+
+    with requests_mock.Mocker() as rm:
+        rm.get(
+            "http://example.com/statuses",
+            status_code=500,
+            json={
+                'status': 'error',
+                'data': []
+            }
+        )
+
+        response_json = communication_management.get_read_messages("c3b8e0c4-5f3d-4a2b-8c7f-1a2e9d6f3b5c")
+
+        assert response_json["status"] == "error"
+        assert response_json["data"] == []
