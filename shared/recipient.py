da1b84a3fa3145603708aa996bddc27dd0665451
Steve Laing <steve.laing@gmail.com>
2025-06-03 14:45:47 +0100

    Remove unused fields from Recipient named tuple

--- shared/recipient.py
+++ shared/recipient.py
@@ -9,11 +9,3 @@ class Recipient(NamedTuple):
     batch_id: str | None = None
     routing_plan_id: str | None = None
     message_status: str | None = None
-    variable_text_1: str | None = None
-    address_line_1: str | None = None
-    address_line_2: str | None = None
-    address_line_3: str | None = None
-    address_line_4: str | None = None
-    address_line_5: str | None = None
-    postcode: str | None = None
-    gp_practice_name: str | None = None
