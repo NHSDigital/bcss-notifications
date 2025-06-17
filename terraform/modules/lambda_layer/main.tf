51fc4fea620e390a0241fd10164b81de2dd0d6a1
Steve Laing <steve.laing@gmail.com>
2025-06-17 13:33:10 +0100

    Deploy a placeholder zip for dependencies lambda layer resource

--- terraform/modules/lambda_layer/main.tf
+++ terraform/modules/lambda_layer/main.tf
@@ -1,23 +1,19 @@
 locals {
-  runtime       = "python3.13"
-  project_root  = "${path.module}/../../.."
-  build_trigger = sha256(timestamp())
+  runtime = "python3.13"
 }
 
-resource "null_resource" "packages_zipfile" {
-  provisioner "local-exec" {
-    command     = "./build.sh"
-    working_dir = path.module
-  }
-  triggers = {
-    always_run = local.build_trigger
+data "archive_file" "placeholder_zip" {
+  type        = "zip"
+  output_path = "${path.module}/placeholder.zip"
+
+  source {
+    content  = "placeholder"
+    filename = "placeholder.txt"
   }
 }
 
 resource "aws_lambda_layer_version" "python_packages" {
-  depends_on          = [null_resource.packages_zipfile]
   layer_name          = "${var.team}-${var.project}-python-packages-${var.environment}"
   compatible_runtimes = [local.runtime]
-  filename            = "${path.module}/packages.zip"
-  source_code_hash    = local.build_trigger
+  filename            = "${path.module}/placeholder.zip"
 }
