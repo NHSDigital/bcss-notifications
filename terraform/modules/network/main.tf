6e9bdf2e3e26365a3f5a215310f52f6322789db7
gdcNHS <133769288+gdcNHS@users.noreply.github.com>
2025-08-08 13:29:00 +0100

    Update subnet config

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -2,6 +2,9 @@ data "aws_vpc" "selected" {
   id = var.selected_vpc_id
 }
 
+# Important note:
+# The DB requires connection only from private-a subnet.
+# This is because the DB is only available in AZ 2a
 data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
@@ -9,7 +12,7 @@ data "aws_subnets" "private" {
   }
 
   tags = {
-    "Name" = "*private*"
+    "Name" = "*private-a*"
   }
 }
 
