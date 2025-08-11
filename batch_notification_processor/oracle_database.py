144190fd116a41cba9230a84a4fa6680845b28f7
Uzair Haroon <uzair.haroon1@nhs.net>
2025-08-11 17:19:11 +0100

    Reverted change to remove columns from SELECT query

--- batch_notification_processor/oracle_database.py
+++ batch_notification_processor/oracle_database.py
@@ -31,6 +31,9 @@ def get_recipients(batch_id: str) -> list[Recipient]:
                 """
                 SELECT nhs_number,
                        message_id,
+                       batch_id,
+                       routing_plan_id,
+                       message_status,
                        address_line_1,
                        address_line_2,
                        address_line_3,
