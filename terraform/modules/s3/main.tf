ca94573d8147768d637bf7a67dffaed406a87426
gdcNHS <gregg.cowell2@nhs.net>
2025-08-15 12:12:34 +0100

    Modify bucket rules

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,5 +1,4 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  count  = var.environment != "prod" ? 1 : 0
   bucket = "${var.team}-${var.project}-s3-${var.environment}"
   tags   = var.tags
 }
--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-  value = aws_s3_bucket.s3_bucket[0].id
+  value = aws_s3_bucket.s3_bucket.id
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.s3_bucket[0].arn
+  value = aws_s3_bucket.s3_bucket.arn
 }
